a230946862c0a75261c90936ad1dfc8ce0a83db2
unknown <j0s3.90jr@gmail.com>
2019-12-17 18:19:57 +0100

    Add Model to Controllers

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pokemon;
 
 class PokemonController extends Controller
 {
--- app/Http/Controllers/PokemonregionController.php
+++ app/Http/Controllers/PokemonregionController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\PokemonRegion;
 
 class PokemonregionController extends Controller
 {
+    // Function to read all registers from PokemonRegion's table.
     public function index(){
         $pokemonRegion = PokemonRegion::all();
         return $pokemonRegion;
     }
+    // Function to insert a register from PokemonRegion's table.
     public function store(Request $request){
         $pokemonRegion = new PokemonRegion;
 
@@ -17,6 +20,7 @@ class PokemonregionController extends Controller
         $pokemonRegion->regioniD = $request->regioniD;
         $pokemon->save;
     }
+    // Function to update a register from PokemonRegion's table.
     public function update(Request $request){
         $pokemonTipo = new Pokemon;
 
@@ -24,6 +28,7 @@ class PokemonregionController extends Controller
         $pokemonTipo->regioniD = $request->regioniD;
         $pokemonTipo->save;
     }
+    // Function to delete a register from PokemonRegion's table.
     public function destroy(Request $request){
         $pokemonTipo = new PokemonIpo;
 
